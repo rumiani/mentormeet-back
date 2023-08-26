9df6c19ec3c8ea800a242ac2961c1c1ad7f3971c
rumiani <maziar.rumiani@gmail.com>
2023-08-26 23:06:53 +0330

    credential sent to the server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import './db/mongoose.js'
 import express from 'express';
 import userRouter from './routers/user.js'
-
+import cors from 'cors';
 const app = express();
+app.use(express.json())
 
 const port = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+
 app.use(userRouter)
 
-app.use(express.json())
 
 
 app.listen(port, ()=> {
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,19 +2,26 @@ import express from 'express';
 import User from '../models/user.js';
 
 const userRouter = new express.Router()
-
-userRouter.get('/', (req, res)=>{
-    res.send('Heldlo, this is Express + TypeScript');
+userRouter.post('/login', async (req, res)=>{
+  const username = req.body;
+  console.log(username);
+  try {
+    const user = await User.findOne({ "username":username })
+    if(!user) return res.status(404).send('User not found');
+    res.send(`<h1>${user.username}<h1>`)
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 
 userRouter.post('/', (req, res)=>{
-    User.find(req.body).then( () =>{
-      res.send(user)
-    }).catch((err) =>{
-      res.status(400)
-      res.send(err)
-    })
+    // User.find(req.body).then( () =>{
+    //   res.send(user)
+    // }).catch((err) =>{
+    //   res.status(400)
+    //   res.send(err)
+    // })
 });
 userRouter.get('/mentors/:username', async(req,res)=>{
   const username = req.params.username;
@@ -28,6 +35,7 @@ userRouter.get('/mentors/:username', async(req,res)=>{
 })
 
 userRouter.get('/mentors', async(req,res)=>{
+  console.log(req.body);
   try {
     const mentors = await User.find({ role: 'mentor' })
     res.send(mentors)
